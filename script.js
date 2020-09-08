57a4c209a7ff4bbcf84af9416cdafef700a11e86
Arafat Hossain Ar <arafat.122260@gmail.com>
2020-09-09 00:10:11 +0600

    added time and date js

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ const signIn = document.getElementById("signin");
 const signUp = document.getElementById("signup");
 const toLogin = document.getElementById("login-link");
 const toCreate = document.getElementById("create-link");
+const date = document.getElementById("date");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const weekday = document.getElementById("weekday");
 const hide = "none";
 const show = "block"
 const flex = "flex";
+
 function noneItem() {
     formHeader.innerHTML = "Registration" + "<hr>";
     loginBtn.style.display = hide;
@@ -21,13 +26,55 @@ function noneItem() {
     signIn.style.display = show;
 
 }
+function timeDate() {
+
+    const dates = new Date();
+
+    const myDate = dates.getDate();
+    var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    const getMonth = months[dates.getMonth()];
+    const getYear = dates.getFullYear();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    const getWeekday = days[dates.getDay()];
+
+
+    date.innerText = myDate;
+    month.innerText = getMonth;
+    year.innerText = getYear;
+    weekday.innerText = getWeekday;
+
+
+
+}
+
+timeDate();
 noneItem();
 
-toCreate.addEventListener("click", (e) =>{
-
- noneItem();
+toCreate.addEventListener("click", (e) => {
+    noneItem();
 });
-toLogin.addEventListener("click", (e) =>{
+toLogin.addEventListener("click", (e) => {
 
     formHeader.innerHTML = "Login" + "<hr>";
     createBtn.style.display = hide;
@@ -36,7 +83,4 @@ toLogin.addEventListener("click", (e) =>{
 
     loginBtn.style.display = show;
     signUp.style.display = show;
-    
-
-
 });
