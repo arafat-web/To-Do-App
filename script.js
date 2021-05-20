aeb34b88b14a54b7b303721673e1a0296e4bd475
Arafat Hossain Ar <arafat.122260@gmail.com>
2021-05-20 23:00:15 +0600

    changes

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 
 'use strict';
 window.addEventListener('load', (event) => {
-
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCpQW3dkjGxqTPGhXcvRCQ__4UViQBDq28",
+    authDomain: "to-do-app-5b1c1.firebaseapp.com",
+    databaseURL: "https://to-do-app-5b1c1.firebaseio.com",
+    projectId: "to-do-app-5b1c1",
+    storageBucket: "to-do-app-5b1c1.appspot.com",
+    messagingSenderId: "756757679800",
+    appId: "1:756757679800:web:0e2789899a508626fa2e4d",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const fs = firebase.firestore();
+    //DOM ELEMENTS//
+    ///////////////////////////////////
     const formHeader = document.getElementById("form-header");
     const userName = document.getElementById("username");
     const emailId = document.getElementById("email");
-    const passWord = document.getElementById("password");
     const usrnm = document.getElementById("usrnm");
     const mail = document.getElementById("mail");
     const psw = document.getElementById("psw");
@@ -26,10 +40,23 @@ window.addEventListener('load', (event) => {
     const todoAdd = document.getElementById("todoadd-btn");
     const todoContainer = document.getElementById("todo-container");
     const Logout = document.getElementById("logout-btn");
+    const Load = document.getElementById("loader");
+    const main = document.getElementById("main");
+    const toast = document.getElementById("toast");
+    /////////////////////////////////
+
+    //Basic DOM elements//
     const hide = "none";
     const show = "block";
     const flex = "flex";
-
+    //Loader//
+    function showPage() {
+        Load.style.display = hide;
+        main.style.display = show;
+    }
+    //Show loader
+    setTimeout(showPage, 3000);
+    //Basic create account section
     function noneItem() {
         formHeader.innerHTML = "Hello There!<br>Before using this you need to create an account.";
         loginBtn.style.display = hide;
@@ -40,8 +67,8 @@ window.addEventListener('load', (event) => {
         signIn.style.display = show;
 
     }
+    //Get time and date
     function timeDate() {
-
         const dates = new Date();
 
         const myDate = dates.getDate();
@@ -59,6 +86,7 @@ window.addEventListener('load', (event) => {
             "November",
             "December"
         ];
+
         const getMonth = months[dates.getMonth()];
         const getYear = dates.getFullYear();
         let days = [
@@ -71,8 +99,6 @@ window.addEventListener('load', (event) => {
             "Saturday"
         ];
         const getWeekday = days[dates.getDay()];
-
-
         date.innerText = myDate;
         month.innerText = getMonth;
         year.innerText = getYear;
@@ -81,13 +107,11 @@ window.addEventListener('load', (event) => {
 
 
     }
-
-    timeDate();
-    noneItem();
-
+    //Basic AC Creation fom
     toCreate.addEventListener("click", (e) => {
         noneItem();
     });
+    //Basic AC login form
     toLogin.addEventListener("click", (e) => {
 
         formHeader.innerHTML = "Hello There!<br>Please login using to see your contents.";
@@ -99,13 +123,18 @@ window.addEventListener('load', (event) => {
         loginBtn.style.display = show;
         signUp.style.display = show;
     });
-
+    //Account creation
     createBtn.addEventListener('click', (e) => {
         e.preventDefault()
         const name = usrnm.value;
         const email = mail.value;
         const password = psw.value;
-
+        load = 2;
+        if (name === '' || email === '' || password === '') {
+            toast.innerText = "Please fill out fields";
+            toast.className = "show";
+            setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 3000);
+        }
         auth.createUserWithEmailAndPassword(email, password).then(cred => {
             return fs.collection('users').doc(cred.user.uid).set({
                 Name: name,
@@ -114,17 +143,23 @@ window.addEventListener('load', (event) => {
             }).then(() => {
                 profile.style.display = show;
                 form.style.display = hide;
+                usrnm.value = "";
+                mail.value = "";
+                psw.value = "";
             }).catch(err => {
-                console.log(err.message);
+                toast.innerText = err.message;
+                toast.className = "show";
+                setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 3000);
             })
 
         }).catch(err => {
-            console.log(err.message);
+            // toast.innerText = err.message;
+            // toast.className = "show";
+            // setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
         })
-        usrnm.value = "";
-        mail.value = "";
-        psw.value = "";
+
     })
+    //Loagin to Account
     loginBtn.addEventListener("click", (e) => {
         e.preventDefault();
 
@@ -135,14 +170,19 @@ window.addEventListener('load', (event) => {
 
             profile.style.display = show;
             form.style.display = hide;
+            mail.value = "";
+            psw.value = "";
+        }).catch(err => {
+            toast.innerText = err.message;
+            toast.className = "show";
+            setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 3000);
+        })
 
-        });
 
-        mail.value = "";
-        psw.value = "";
     });
 
 
+    //what will be happend after logged in
 
     auth.onAuthStateChanged(user => {
         if (user) {
@@ -155,11 +195,17 @@ window.addEventListener('load', (event) => {
         }
     });
 
+    //adding todos
     const datess = new Date();
     const time = datess.getTime();
     let counter = time;
     todoAdd.addEventListener("click", (e) => {
         const todos = todo.value;
+        if( todos === ''){
+            toast.innerText = "Empty Field";
+            toast.className = "show";
+            setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 3000);
+        }else{
         let id = counter += 1;
         auth.onAuthStateChanged(user => {
             if (user) {
@@ -167,16 +213,20 @@ window.addEventListener('load', (event) => {
                     id: '_' + id,
                     todos
                 }).then(() => {
-                    console.log("added")
+                    toast.innerText = "Added Successfully";
+                    toast.className = "show";
+                    setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 3000);
                 }).catch(err => {
-                    console.log(err.message);
+                    toast.innerText = err.message;
+                    toast.className = "show";
+                    setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 3000);
                 })
             }
         })
         todo.value = "";
-
+    }
     });
-
+    //Create todos in section
     function renderData(individualDoc) {
 
         let parentDiv = document.createElement("div");
@@ -205,18 +255,31 @@ window.addEventListener('load', (event) => {
                     fs.collection(user.uid).doc(id).delete();
                 }
             })
+            toast.innerText = "Content Deleted";
+            toast.className = "show";
+            setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 3000);
 
         })
     }
-    auth.onAuthStateChanged(user =>{
-        if(user){
+    //is user logged in or out?
+    auth.onAuthStateChanged(user => {
+        if (user) {
 
-        } else{
-            alert("Please Login or Create Account to continue");
+        } else {
+            if (logValue === 1) {
+                al();
+            }
+            else {
+
+            }
+            function al() {
+                alert("Please Login or Create Account to continue");
+            }
             profile.style.display = hide;
-            form.style.display = show;
+            main.style.display = show;
         }
     })
+    //Real time database
     auth.onAuthStateChanged(user => {
         if (user) {
             fs.collection(user.uid).onSnapshot(snapshot => {
@@ -232,7 +295,13 @@ window.addEventListener('load', (event) => {
             })
         }
     })
-    Logout.onclick =  () =>{
+    //logged out
+    let logValue;
+    Logout.onclick = () => {
+        logValue = 1;
         auth.signOut();
     };
+    //function calling
+    timeDate();
+    noneItem();
 });
